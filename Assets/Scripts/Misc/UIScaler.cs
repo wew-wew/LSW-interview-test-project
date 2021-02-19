3fe2e2a504acfeed73aacb2fd0cf45c368ff6c62
wew-wew <50919041+wew-wew@users.noreply.github.com>
2021-02-19 05:34:06 +0500

    scripting/minor visual changes

---
+++ Assets/Scripts/Misc/UIScaler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScaler : MonoBehaviour
+{
+    public RectTransform coins_panel_rekt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // figuring out the size of in-game "unit" which is 64 pixels compared to screen coordinates
+        Vector3 screen_point_0 = GetComponent<Canvas>().worldCamera.WorldToScreenPoint(Vector3.zero);
+        Vector3 screen_point_1 = GetComponent<Canvas>().worldCamera.WorldToScreenPoint(new Vector3(1,0,0));
+
+        // camera space unit
+        float csu = screen_point_1.x - screen_point_0.x;
+
+        coins_panel_rekt.anchoredPosition = new Vector2(-csu/2f, csu/2f);
+        coins_panel_rekt.sizeDelta = new Vector2(3 * csu, csu)/coins_panel_rekt.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
